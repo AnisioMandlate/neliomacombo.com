7d6af03dcff87d21e0b0a77fb4bfd7f22e45cbdf
AnisioMandlate <anisiomandlate@gmail.com>
2022-09-02 15:26:21 +0530

    feat:Add Certifications and Education sections on About page

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -8,9 +8,9 @@ export default function About() {
         <title>About Me - Nélio Macombo | Product Manager</title>
       </Head>
 
-      <div className="mt-8 flex flex-col justify-center min-h-[100vh]">
-        <div className="mt-8">
-          <h1 className="text-4xl md:text-5xl font-serif text-title dark:text-white font-bold mb-5">
+      <div className="my-8 flex flex-col justify-center min-h-[100vh]">
+        <section className="mt-8">
+          <h1 className="text-2xl md:text-3xl font-serif text-title dark:text-white font-bold mb-5 uppercase">
             About Me
           </h1>
 
@@ -26,31 +26,72 @@ export default function About() {
 
           <ul className="mt-8 pl-8 text-lg leading-[150%] md:leading-9 max-w-[100%] font-sans">
             <li className="mt-6">
-              •<b>Product Strategy:</b> I Frame a story starting with the
+              <b>• Product Strategy:</b> I Frame a story starting with the
               problem, quantify it to show the opportunity, then arrive at the
               solution.
             </li>
             <li className="mt-6">
-              •<b>Product Development:</b> I solve problems and provide
+              <b>• Product Development:</b> I solve problems and provide
               solutions using experiments through testing and iterating engaging
-              ideas,
+              ideas.
             </li>
             <li className="mt-6">
-              •<b>Product Marketing:</b> I coordinate with the Brand, Marketing,
-              and Design Team to launch whether for a small base or big customer
-              base using a customer-centric approach, where I influence my team
-              to put themselves in the customer's shoes and work backward to
-              plan the unexpected.
+              <b>• Product Marketing:</b> I coordinate with the Brand,
+              Marketing, and Design Team to launch whether for a small base or
+              big customer base using a customer-centric approach, where I
+              influence my team to put themselves in the customer's shoes and
+              work backward to plan the unexpected.
             </li>
             <li className="mt-6">
-              •<b>Product Team and Stakeholders alignment:</b> The Team is the
+              <b>• Product Team and Stakeholders alignment:</b> The Team is the
               how of every product or project success. I Set early the goals for
               the team and the principles behind them, and then bring everyone
               together through the artifacts, frequent feedback, and
               communication.
             </li>
           </ul>
-        </div>
+        </section>
+
+        <section className="mt-8">
+          <h1 className="text-2xl md:text-3xl font-serif text-title dark:text-white font-bold mb-5 uppercase">
+            Certifications
+          </h1>
+
+          <ul className="mt-8 pl-8 text-lg leading-[150%] md:leading-9 max-w-[100%] font-sans">
+            <li className="mt-6">
+              <b>• 2021</b> - Product Management Nanodegree Program from Udacity
+            </li>
+            <li className="mt-6">
+              <b>• 2021</b> - Marketing Analytics Nanodegree Program from
+              Udacity
+            </li>
+            <li className="mt-6">
+              <b>• 2021</b> - Agile Project Manager Certification from LinkedIn
+              Learning
+            </li>
+            <li className="mt-6">
+              <b>• 2018</b> - Web Mobile Specialization from Andela Learning
+              Community powered by Google
+            </li>
+            <li className="mt-6">
+              <b>• 2015</b> - Specialization in Software Engineering and Digital
+              Business from C.E.S.A.R EDU, Brazil
+            </li>
+          </ul>
+        </section>
+
+        <section className="mt-8">
+          <h1 className="text-2xl md:text-3xl font-serif text-title dark:text-white font-bold mb-5 uppercase">
+            Education
+          </h1>
+
+          <ul className="mt-8 pl-8 text-lg leading-[150%] md:leading-9 max-w-[100%] font-sans">
+            <li className="mt-6">
+              <b>• 2016</b> - Bachelors in Computer Engineering from University
+              Eduardo Mondlane
+            </li>
+          </ul>
+        </section>
       </div>
     </Fragment>
   );
