b98a4f8359e4c4e6c64cc8df0803cd5b09d58831
AnisioMandlate <anisiomandlate@gmail.com>
2022-10-01 15:00:52 +0530

    feat: Add Google Analytics Integration

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/global.css";
+import Script from "next/script";
 import { ThemeProvider } from "next-themes";
 import { Footer } from "../components/Footer";
 import { Navbar } from "../components/Navbar";
@@ -6,6 +7,21 @@ import { Navbar } from "../components/Navbar";
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_KEY}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_KEY}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
       <div className="grid grid-rows-mobile md:grid-rows-app min-h-screen justify-items-center pt-32">
         <Navbar />
         <main className="max-w-5xl w-full px-10 md:px-12 h-full my-auto">
